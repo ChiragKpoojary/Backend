4cc1b5e9c3ae888a85859731874bbdceea50bff1
chirag_k_poojary <chiragkpoojary30@gmail.com>
2024-08-04 15:56:14 +0530

    first controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,18 @@
 import {asynchandler} from '../utils/asynchandler.js';
 
 const registerUser =asynchandler(async (req,res)=>{
-    res.status(200).json({
-        message:"chirag"
-    })
+   //take userdetails
+   //validation
+   //check if user already exists :email
+   //check for avatar
+   //upload cloudinary
+   // create use object - create entry in db
+   //remove password and refresh token feild from response
+   //check for user creation
+   //return response
+
+
+
 })
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/user.controller.js'; // Adjust the path as needed
+import { registerUser } from '../controllers/user.controller.js'; // Adjust the path as needed for the file
 
 const router = Router();
-
+console.log("hi my name is chirag");
 router.post('/register', registerUser);
 
 export { router as userRouter };
